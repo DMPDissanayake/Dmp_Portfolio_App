b80a68bf63a2a3c88734695b5297fa1fd190a2cc
DMPDissanayake <99disanayake@gmail.com>
2026-06-27 09:30:02 +0200

    Folder Stracture setup

---
+++ android/app/src/main/kotlin/com/example/dmpportfolioapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dmpportfolioapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:dmpportfolioapp/app/dmpportfolioapp.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const DmpPortfolioApp());
+}
